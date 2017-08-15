4a5cdfe7faa6d000b6670f50bb8892cfe65a0324
Ian Rees <ian@ianrees.net>
2017-08-15 15:05:46 -0700

    setup.py / requirements.txt

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+import transitlandflows
+
+setup(
+  name='transitlandflows',
+  version='0.1',
+  description='Transitland Flows: Visualization',
+  author='Will Geary',
+  author_email='willcgeary@gmail.com',
+  url='https://github.com/transitland/transitland-processing-animation',
+  license='License :: OSI Approved :: MIT License',
+  packages=['transitlandflows']
+)
